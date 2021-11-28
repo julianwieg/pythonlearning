77abce17b83d32b75badbd91d6194265abae1572
julianwieg <julianwieg@users.noreply.github.com>
2021-11-28 21:15:16 +0100

    some changes

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
 text = "Hello Word"
 print(text)
+
+#f string  can print different types as strings
+how_many_new_mess = 2
+print(f"{how_many_new_mess} new messages")
+
+#If else 
+if True:
+  print("true is statement")
