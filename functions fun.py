df9ea8f1630dd7d521d2869509c24f0bfeec3129
Julian Wiegmann <julianwieg@users.noreply.github.com>
2021-12-21 22:26:30 +0100

    done for xmas

--- functions fun.py
+++ functions fun.py
@@ -1,4 +1,4 @@
-# basic of functions
+# basic of functions.  to use call the function
 def age_label(age):
   label = "my age " + age
   return label
@@ -11,12 +11,13 @@ print(results)
 def is_freezing(temp):
   return temperature < 0
 
+# give good names
 def sum_score(score, bonus):
     print(score + bonus)
 
 sum_score(100,50)
 
-# the parameter in the condition (function) we pass when calling the function like 80
+# the parameter (cart here) in the condition (function) we pass when calling the function -> here 80
 def add_shipping(cart):
     if cart < 100:
         print(f"Total: {cart + 10}")  #returns Total: 90
@@ -28,8 +29,28 @@ def calculator(operator, x, y):
     if operator == "+":
         print(x+y)
     elif operator == "-":
-        print(x - y)
+        print(x - y) 
     else:
         print(f"unknown: {operator}")
 
 calculator("-", 30, 20)
+
+# while loops with functions
+def onboard_passenger(bookings):
+    counter = 1
+    while counter <= bookings:
+        print(f"Passenger {counter} added.")
+        counter += 1
+
+onboard_passenger(4)
+
+# for loops 
+def display_progress(total_files):
+    for i in range(total_files):
+        print(f"File {i} out of {total_files}")
+
+display_progress(3)
+
+
+
+
---
+++ tuples and strings.py
@@ -0,0 +1,27 @@
+# tuples vs strings
+# tuples use ()    and lists []   tuples need a , at the end
+# tuples are IMMUTABLE  
+# you can store tuples inside a list
+
+score = [("mia", 75), ("lee", 90)]
+print(score)
+print(len(score))  #cause it is two tuples in the list it is 2
+mia_score = score[0] #access tuple 1 (0)
+print(mia_score)
+
+for user_score in score:        #or iterate through
+    print(f"Result {user_score}")
+
+# to store a typle returned by a function assign a function calls result to variable like results
+def get_highlow(values_list):
+    high_value = max(values_list)
+    low_value = min(values_list)
+    return high_value, low_value
+
+values = [32, 13, 90,222]
+results = get_highlow(values)
+print(results)
+print(type(results))
+#but to return individual values use indices
+highest = results[0]
+print(F"highest value {highest}")
